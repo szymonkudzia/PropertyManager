489619a411134f1e6906d9c2fb14911751eb1daa
szymonkudzia <szymonkudzia@gmail.com>
2015-03-23 21:47:45 +0100

    Added parsing of property doc Added finding default value of property (@default in pdoc) Small fix in styling of description div (white-space: pre)

--- src/com/sk/property_manager/PropertiesExtractor.java
+++ src/com/sk/property_manager/PropertiesExtractor.java
@@ -1,5 +1,7 @@
 package com.sk.property_manager;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,6 +12,8 @@ import java.util.List;
  * Created by Szymon on 2015-03-15.
  */
 public class PropertiesExtractor {
+    private static final String DEFAULT = "@default";
+
     public List<Property> extract(List<File> propertyFiles) {
         List<Property> properties = new ArrayList<Property>();
 
@@ -30,11 +34,42 @@ public class PropertiesExtractor {
         try {
             List<String> lines = Files.readAllLines(propertyFile.toPath());
 
+            StringBuilder description = new StringBuilder();
+            boolean isPDoc = false;
+
             for (String line : lines) {
                 System.out.println("Current line: " + line);
-                String[] values = line.split("=", -1);
+                line = line.trim();
 
-                properties.add(new Property(values[0], values[1], "", "none"));
+                if (line.startsWith("##")) {
+                    isPDoc = true;
+                    description
+                            .append(line.substring(2))
+                            .append("\n");
+
+                } else if (line.startsWith("#") && isPDoc) {
+                    description
+                            .append(line.substring(1))
+                            .append("\n");
+
+                } else if (!StringUtils.isBlank(line)) {
+                    isPDoc = false;
+
+                    String defaultValue = "";
+
+                    int defaultStart = description.indexOf(DEFAULT) + DEFAULT.length();
+                    if (defaultStart >= 8) {
+                        int lineEnd = description.indexOf("\n", defaultStart);
+
+                        defaultValue = description.substring(defaultStart, lineEnd);
+                    }
+
+                    String[] values = line.split("=", -1);
+                    properties.add(new Property(values[0], values[1], defaultValue, description.toString()));
+
+                    description.setLength(0);
+                }
+
             }
 
         } catch (IOException e) {
